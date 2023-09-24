c609b24cff588c03924ee414077001599e05a163
JuanQP <quinterosp.juan@gmail.com>
2023-09-24 15:08:32 -0300

    Refactor

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,3 +1,7 @@
+import cv2
+
+RED_COLOR = (0, 0, 255)
+
 COLORS = {
     'pelota': {
         'text': (0, 0, 0),
@@ -34,3 +38,34 @@ VIDEOS_FOLDER = "videos"
 
 def image_to_rgb(image):
     return image[..., ::-1]
+
+def to_detection(row):
+    detection_name = row['name']
+
+    return {
+        "name": detection_name,
+        "confidence": row['confidence'],
+        "start": (int(row['xmin']), int(row['ymin'])),
+        "end": (int(row['xmax']), int(row['ymax'])),
+        "colors": COLORS[detection_name],
+    }
+
+def draw_detection(image, detections_by_type, detection):
+    start_point = detection["start"]
+    detection_name = detection["name"]
+    confidence = detection["confidence"]
+    colors = detection["colors"]
+    is_greater_than_max_count = detections_by_type[detection_name] > MAX_AMOUNTS[detection_name]
+
+    # Some positioning for the label and background...
+    label_position = (start_point[0], start_point[1] - 10)
+    label_text = f"{detection_name} {round(confidence * 100)}%"
+    (width, height), _ = cv2.getTextSize(label_text, cv2.FONT_HERSHEY_SIMPLEX, 0.8, 2)
+    label_background_end_position = (start_point[0] + width, start_point[1] - height * 2)
+    border_color = RED_COLOR if is_greater_than_max_count else colors['background']
+
+    # Draw a rectangle
+    cv2.rectangle(image, detection["start"], detection["end"], border_color, thickness=2)
+    # Draw text in image with background
+    cv2.rectangle(image, detection["start"], label_background_end_position, border_color, thickness=-1)
+    cv2.putText(image, label_text, label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.8, detection['colors']['text'], 2)
